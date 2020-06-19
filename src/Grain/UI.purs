4742771f8d6ccb707feef5458a452e1e55e2c266
Shinya Takahashi <s.takahashi313@gmail.com>
2020-06-19 15:34:18 +0900

    Move Render module into UI module

--- src/Grain/UI.purs
+++ src/Grain/UI.purs
@@ -14,11 +14,17 @@ module Grain.UI
   , didCreate
   , didUpdate
   , didDelete
+  , Render
+  , Query
+  , runRender
+  , useValue
+  , useUpdater
   ) where
 
 import Prelude
 
-import Control.Monad.Reader (ReaderT, ask, local, runReaderT)
+import Control.Monad.Reader (ReaderT, ask, local, runReaderT, withReaderT)
+import Control.Monad.Rec.Class (class MonadRec)
 import Data.Array (take, (!!), (:))
 import Data.Foldable (sequence_)
 import Data.Maybe (Maybe(..), fromMaybe)
@@ -29,7 +35,6 @@ import Effect.Class (liftEffect)
 import Effect.Ref (Ref, modify, modify_, new, read, write)
 import Foreign.Object (Object, empty, insert)
 import Grain.Class (class Grain, which)
-import Grain.Render (Render, runRender)
 import Grain.Store (Store, createStore, readGrain, subscribeGrain, unsubscribeGrain, updateGrain)
 import Grain.Styler (Styler, mountStyler, unmountStyler)
 import Grain.UI.Diff (class HasKey, diff)
@@ -223,6 +228,56 @@ didDelete
 didDelete handler (VNode k (VElement r)) =
   VNode k $ VElement r { didDelete = handler }
 didDelete _ velement = velement
+
+
+
+-- | The type of component renderer.
+-- |
+-- | In this monad, you can declare that you use some states and updaters.
+newtype Render a = Render (ReaderT QueryBox Effect a)
+
+-- Do not derive MonadEffect
+derive newtype instance functorRender :: Functor Render
+derive newtype instance applyRender :: Apply Render
+derive newtype instance applicativeRender :: Applicative Render
+derive newtype instance bindRender :: Bind Render
+derive newtype instance monadRender :: Monad Render
+derive newtype instance semigroupRender :: Semigroup a => Semigroup (Render a)
+derive newtype instance monoidRender :: Monoid a => Monoid (Render a)
+derive newtype instance monadRecRender :: MonadRec Render
+
+newtype QueryBox = QueryBox Query
+
+type Query =
+  { selectValue :: forall p a. Grain p a => p a -> Effect a
+  , updateValue :: forall p a. Grain p a => p a -> (a -> a) -> Effect Unit
+  }
+
+runRender :: forall a. Render a -> Query -> Effect a
+runRender (Render reader) = runReaderT reader <<< QueryBox
+
+-- | Listen a state, then return it.
+-- |
+-- | If the state is changed, the component will be rerendered.
+useValue
+  :: forall p a
+   . Grain p a
+  => p a
+  -> Render a
+useValue proxy = Render do
+  QueryBox query <- ask
+  withReaderT (const query)
+    $ liftEffect $ query.selectValue proxy
+
+-- | Get an updater of a state.
+useUpdater
+  :: forall p a
+   . Grain p a
+  => p a
+  -> Render ((a -> a) -> Effect Unit)
+useUpdater proxy = Render do
+  QueryBox query <- ask
+  pure $ query.updateValue proxy
 
 
 
