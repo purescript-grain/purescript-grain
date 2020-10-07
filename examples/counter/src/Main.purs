ba6e322f7aa4243820fbc01b0d950f2808e94cfe
Shinya Takahashi <s.takahashi313@gmail.com>
2020-10-08 00:32:57 +0900

    Tiny fix

--- examples/counter/src/Main.purs
+++ examples/counter/src/Main.purs
@@ -34,9 +34,9 @@ instance localGrainCount :: LocalGrain Count where
 view :: VNode
 view = H.component do
   count <- useValue (LProxy :: _ Count)
-  updateItem <- useUpdater (LProxy :: _ Count)
-  let increment = updateItem $ over Count (_ + 1)
-      decrement = updateItem $ over Count (_ - 1)
+  updateCount <- useUpdater (LProxy :: _ Count)
+  let increment = updateCount $ over Count (_ + 1)
+      decrement = updateCount $ over Count (_ - 1)
   pure $ H.div # H.css containerStyles # H.kids
     [ H.button
         # H.css buttonStyles
